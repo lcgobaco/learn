9bd615064c4b36fc430c65e250092ce43009c256
lucas <lucas@gobaco.com>
2020-08-29 13:37:58 -0700

    Linked List Data Structure

---
+++ data-structures/src/main/java/com/lucas/LinkedListNode.java
@@ -0,0 +1,10 @@
+package com.lucas;
+
+public class LinkedListNode {
+    String value;
+    LinkedListNode nextNode;
+    public LinkedListNode(String value, LinkedListNode nextNode) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}
